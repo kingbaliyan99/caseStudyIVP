9ebb9c8465a8e05c4a047f01670dfd19f37c913e
Harshita Garg <hagarg@ivp.in>
2024-02-15 20:48:28 +0530

    added backend folder

--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import ColorRadioButtons from './Components/ColorRadioButtons';
+
+import TabbedAttributes from './Components/TabedAttributes';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Import BrowserRouter
 
 function App() {
   return (
+    
     <div className="App">
-      <p>page</p>
+     <TabbedAttributes/>
     </div>
   );
 }
